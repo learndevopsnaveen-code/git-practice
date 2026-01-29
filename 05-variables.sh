bc8d9f29e6d26b6fa353dc8e2c15bef235b823b5
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 10:39:14 +0530

    added uername and password

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 
-echo "please enter user name"
+echo "please enter user name::"
 
-read USERNAME
+read -s USERNAME #takes input into username variable
+
 
 echo "USERNAME entered is : $USERNAME"
+
+echo "please enter user password::"
+
+read -s PASSWORD
+
+echo "PASSWORD entered is : $PASSWORD"
