8312763420fcd1535cabc6cc77f98b83ecabce0b
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 12:13:31 +0530

    added arrays

--- 06-array.sh
+++ 06-array.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# index starts from 0, size is 3
-FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+# index starts from 0, size is 4
+FRUITS=("APPLE" "KIWI" "ORANGE" "LEMON") #Array
 
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
+echo "Fourth fruit is : ${FRUITS[3]}"
 
 echo "First fruit is: ${FRUITS[@]}"
