a2537817a3ecd6c3ea6fc1c95c6f7331a59ff951
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 10:28:45 +0530

    changed the names

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=suresh
+PERSON1=Naveen
+PERSON2=Mohan
 echo "$PERSON1 :: hi $PERSON2 how are you"
 echo "$PERSON2 ::hello $PERSON1 iam fine , how are you doing"
 echo "$PERSON1 :: iam doing good $PERSON2, what is going on"
