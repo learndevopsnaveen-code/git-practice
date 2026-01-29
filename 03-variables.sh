45065d0557a0da061b736815b3788dd15453be15
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 10:25:17 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=suresh
+echo "$PERSON1 :: hi $PERSON2 how are you"
+echo "$PERSON2 ::hello $PERSON1 iam fine , how are you doing"
+echo "$PERSON1 :: iam doing good $PERSON2, what is going on"
+echo "$PERSON2 :: i started learning shell script $PERSON1"
