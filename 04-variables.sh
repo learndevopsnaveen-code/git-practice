cbffa0fa051e1857d2a0bcb9e0a879877a7adb7d
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 10:30:50 +0530

    changed the names

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :: hi $PERSON2 how are you"
+echo "$PERSON2 ::hello $PERSON1 iam fine , how are you doing"
+echo "$PERSON1 :: iam doing good $PERSON2, what is going on"
+echo "$PERSON2 :: i started learning shell script $PERSON1"
