bfa0844dce88b54f453a2d6eeb0a9642e1572b6d
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-29 10:16:06 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: hi suresh how are you"
+echo "suresh ::hello Ramesh iam fine , how are you doing"
+echo "Ramesh :: iam doing good suresh, what is going on"
+echo "suresh :: i started learning shell script Ramesh"
