0c6fa1c5b4c6994433e5e6eaadb6fe85f2be5725
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-01-31 22:26:53 +0530

    meaning added

--- 00-devops.sh
+++ 00-devops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-What is DevOps?
+echo What is DevOps?
 
-we create the process calld CICD with the help of tools, the same day we are 
-ablt to build and test the application
+echo we create the process calld CICD with the help of tools, the same day we are 
+echo ablt to build and test the application
