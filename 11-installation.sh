01f1106c8642f1570c90dbb332df8a13dc0c60d7
Samala Naveen Kumar <samalanaveen.kumar@clarivate.com>
2026-02-17 06:51:55 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-dnf install git -y
+USERID=$(id -u)
+echo "Uer id is :$USERID"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run the script with root priveleges"
+    exit 1
+fi
+
+dnf list installed git
+
+if[ $? -ne 0 ]
+then 
+    echo "git is not installed , going to install it"
+    dnf install git -y
+else 
+    echo "git already installed , noting to do.."    
+fi    
